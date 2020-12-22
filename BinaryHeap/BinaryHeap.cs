c0a77ab3d4e6d9d7a05cd32635e45cebf91259f4
johnmcerillo <johnmcerillo@gmail.com>
2020-12-21 22:03:35 -0500

    first checkin with four working tests

---
+++ BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace BinaryHeap
+{
+    public class BinaryHeap<T> where T : IComparable
+    {
+        private T[] m_Heap;
+        private int m_Size;
+
+        public BinaryHeap() {
+            m_Heap = new T[8];
+            m_Size = 0;
+        }
+
+        public BinaryHeap(T[] unsortedArr)
+        {
+            m_Heap = unsortedArr;
+            m_Size = unsortedArr.Length;
+            for(var i = (m_Heap.Length) / 2 - 1; 0 <= i; i--)
+            {
+                Heapify(i);
+            }
+        }
+
+        public static void Heapsort(T[] unsortedArr)
+        {
+            var bh = new BinaryHeap<T>(unsortedArr);
+            var i = unsortedArr.Length - 1;
+            while(0 < i)
+            {
+                unsortedArr[i] = bh.Pop();
+                i--;
+            }
+        }
+
+        private static int Left(int i)
+        {
+            return 2 * i + 1;
+        }
+
+        private static int Right(int i)
+        {
+            return 2 * i + 2;
+        }
+
+        private static int Parent(int i)
+        {
+            return (i + 1) / 2 - 1;
+        }
+
+        private void Heapify(int iParent)
+        {
+            int iLeftChild;
+            while ((iLeftChild = Left(iParent)) < m_Size)
+            {
+                var iRightChild = Right(iParent);
+                var iBestChild = m_Size <= iRightChild || m_Heap[iRightChild].CompareTo(m_Heap[iLeftChild]) < 0 ? iLeftChild : iRightChild;
+                var parentObject = m_Heap[iParent];
+                if(0 <= parentObject.CompareTo(m_Heap[iBestChild])) return;
+                m_Heap[iParent] = m_Heap[iBestChild];
+                m_Heap[iBestChild] = parentObject;
+                iParent = iBestChild;
+            }
+        }
+
+        public void Push(T key)
+        {
+            if(m_Heap.Length <= m_Size)
+            {
+                var oldHeap = m_Heap;
+                m_Heap = new T[2 * oldHeap.Length];
+                Array.Copy(oldHeap, m_Heap, oldHeap.Length);
+            }
+
+            m_Heap[m_Size] = key;
+            var iChild = m_Size++;
+
+            while(0 < iChild)
+            {
+                var iParent = Parent(iChild);
+                var parentObj = m_Heap[iParent];
+                if (0 <= parentObj.CompareTo(m_Heap[iChild])) break;
+                m_Heap[iParent] = m_Heap[iChild];
+                m_Heap[iChild] = parentObj;
+                iChild = iParent;
+            }
+        }
+
+        public T Pop()
+        {
+            if(m_Size <= 0) throw new InvalidOperationException("Cannot pop from an empty heap");
+            var root = m_Heap[0];
+            m_Size--;
+            if(m_Size == 0) return root;
+            m_Heap[0] = m_Heap[m_Size];
+            Heapify(0);
+            return root;
+        }
+
+        public T Replace(T key)
+        {
+            if (m_Size <= 0) throw new InvalidOperationException("Cannot replace in an empty heap");
+            var root = m_Heap[0];
+            m_Heap[0] = key;
+            Heapify(0);
+            return root;
+        }
+
+    }
+}
---
+++ BinaryHeapTests/BinaryHeapTests.cs
@@ -0,0 +1,82 @@
+using BinaryHeap;
+using Xunit;
+
+namespace BinaryHeapTests
+{
+    public class BinaryHeapTests
+    {
+        [Fact]
+        public void AscendingSequenceTest()
+        {
+            var bh = new BinaryHeap<int>();
+            var count = 1024;
+            for(var i = 0; i <= count; i++)
+            {
+                bh.Push(i);
+            }
+            for(var i = count; 0 <= i; i--)
+            {
+                Assert.Equal(i, bh.Pop());
+            }
+        }
+
+        [Fact]
+        public void AscendingSequenceWithReplacementTest()
+        {
+            var bh = new BinaryHeap<int>();
+            for(var i = 64; i <= 127; i++)
+            {
+                bh.Push(i);
+            }
+            for(var i = 0; i < 32; i++)
+            {
+                bh.Replace(i);
+            }
+            for(var i = 95; 64 <= i; i--)
+            {
+                Assert.Equal(i, bh.Pop());
+            }
+            for(var i = 31; 0 <= i; i--)
+            {
+                Assert.Equal(i, bh.Pop());
+            }
+        }
+
+        [Fact]
+        public void AscendingSequenceConstructorTest()
+        {
+            var ascending = new int[64];
+
+            for(var i = 0; i < ascending.Length; i++)
+            {
+                ascending[i] = i;
+            }
+
+            var bh = new BinaryHeap<int>(ascending);
+
+            for(var i = ascending.Length - 1; 0 <= i; i--)
+            {
+                Assert.Equal(i, bh.Pop());
+            }
+
+        }
+
+        [Fact]
+        public void HeapsortTest()
+        {
+            var descending = new int[64];
+            for(var i = 63; 0 <= i; i--)
+            {
+                descending[i] = 63 - i;
+            }
+
+            BinaryHeap<int>.Heapsort(descending);
+            var ascending = descending;
+
+            for(var i = 0; i < ascending.Length; i++)
+            {
+                Assert.Equal(i, ascending[i]);
+            }
+        }
+    }
+}
